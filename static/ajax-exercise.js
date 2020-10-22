cc5d5075040dcff112b0057d03023c0c64619662
alexandramilliron <alexandramilliron@gmail.com>
2020-10-22 13:04:30 -0700

    completed showFortune and showWeather functions - partially completed orderMelons function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,18 +4,15 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
+    evt.preventDefault();
 
-    $.get('/fortune', (results) => {
+    $.get("/fortune", (results) => {
         $("#fortune-text").html(results);
     });
 
-    // TODO: get the fortune and show it in the #fortune-text div
 }
 
 $('#get-fortune-button').on('click', showFortune);
-
-
-
 
 
 // PART 2: SHOW WEATHER
@@ -26,13 +23,14 @@ function showWeather(evt) {
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
+    $.get(url, formData, (results) => {
 
-    // TODO: request weather with that URL and show the forecast in #weather-info
+        $("#weather-info").html(results.forecast);
+
+    });
 }
 
 $("#weather-form").on('submit', showWeather);
-
-
 
 
 // PART 3: ORDER MELONS
@@ -42,6 +40,17 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    const formInputs = {
+        'qty': $("#qty-field").val(),
+        'melon': $("#melon-type-field").val(),
+    };
+
+    $.post("/order-melons.json", formInputs, (results) => {
+
+        $("#order-status").html(results.msg, results.code);
+
+    });
 }
 
 $("#order-form").on('submit', orderMelons);
